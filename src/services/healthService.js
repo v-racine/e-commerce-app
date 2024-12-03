06e0d5b06458c7dd9e68a7f847c722bb8a28df08
w-h-a <wesley.herb.anderson@gmail.com>
2024-12-03 15:46:01 -0800

    feat: add health service for demo

---
+++ src/services/healthService.js
@@ -0,0 +1,19 @@
+class HealthService {
+  constructor(args) {
+    this.usersRepo = args.usersRepo;
+  }
+
+  async canRetrieveData() {
+    try {
+      await this.usersRepo.getAll();
+
+      return true;
+    } catch (err) {
+      console.log(`failed to connect to db: `, err.message);
+
+      return false;
+    }
+  }
+}
+
+module.exports = HealthService;
