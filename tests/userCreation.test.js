78adadc0306fd162e9a6f02bda07b4f212b8b28d
w-h-a <wesley.herb.anderson@gmail.com>
2024-12-03 17:10:17 -0800

    test: user creation

---
+++ tests/userCreation.test.js
@@ -0,0 +1,102 @@
+const AppFactory = require('../src/app');
+const request = require('supertest');
+
+describe('user creation', () => {
+  const mockUsersRepo = {};
+
+  const app = AppFactory({
+    usersRepo: mockUsersRepo,
+  });
+
+  describe('when: the db already contains a user with that email', () => {
+    let rsp;
+
+    beforeEach(async () => {
+      mockUsersRepo.getOneBy = jest.fn().mockImplementation(() => {
+        return new Promise((resolve) => {
+          resolve({});
+        });
+      });
+
+      rsp = await request(app)
+        .post('/')
+        .send({ email: 'test1@test.com', password: 'asdf', passwordConfirmation: 'asdf' });
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("then: we return a 'Email in use'", async () => {
+      const status = rsp.status;
+      expect(status).toBe(200);
+
+      const text = rsp.text;
+      expect(text).toBe('Email in use');
+
+      expect(mockUsersRepo.getOneBy).toHaveBeenCalledWith({ email: 'test1@test.com' });
+    });
+  });
+
+  describe('when: the passwords do not match', () => {
+    let rsp;
+
+    beforeEach(async () => {
+      mockUsersRepo.getOneBy = jest.fn().mockImplementation(() => {
+        return new Promise((resolve) => {
+          // is this what is returned when there is no user no matter the repo implementation?
+          resolve(undefined);
+        });
+      });
+
+      rsp = await request(app)
+        .post('/')
+        .send({ email: 'test2@test.com', password: 'asdf', passwordConfirmation: 'asdff' });
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("then: we return 'Password must match'", async () => {
+      const status = rsp.status;
+      expect(status).toBe(200);
+
+      const text = rsp.text;
+      expect(text).toBe('Password must match');
+
+      expect(mockUsersRepo.getOneBy).toHaveBeenCalledWith({ email: 'test2@test.com' });
+    });
+  });
+
+  // TODO: more work needed
+  describe('when: the db does not contain the user and the passwords match', () => {
+    let rsp;
+
+    beforeEach(async () => {
+      mockUsersRepo.getOneBy = jest.fn().mockImplementation(() => {
+        return new Promise((resolve) => {
+          resolve(undefined);
+        });
+      });
+
+      rsp = await request(app)
+        .post('/')
+        .send({ email: 'test3@test.com', password: 'asdf', passwordConfirmation: 'asdf' });
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("then: we return 'Account created!!!'", async () => {
+      const status = rsp.status;
+      expect(status).toBe(200);
+
+      const text = rsp.text;
+      expect(text).toBe('Account created!!!');
+
+      expect(mockUsersRepo.getOneBy).toHaveBeenCalledWith({ email: 'test3@test.com' });
+    });
+  });
+});
