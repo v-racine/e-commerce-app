0df3f874c993d878021144b856f9bba2c239ce53
Valerie Racine <vracine21@gmail.com>
2024-12-16 14:31:53 -0800

    feat: create new products

--- src/views/admin/products/new.js
+++ src/views/admin/products/new.js
@@ -1,14 +1,17 @@
 const layout = require('../layout');
 const { getError } = require('../../../utilities/getErrorHelper');
 
-module.exports = ({ req }) => {
+module.exports = ({ req, errors, submitError }) => {
   return layout({
     content: `
       <form method="POST">
         <input placeholder="Title" name="title" />
+        ${getError(errors, 'title')}
         <input placeholder="Price" name="price" />
+        ${getError(errors, 'price')}
         <input type="file" name="image" />
         <button>Submit</button>
+        ${submitError ? submitError : ''}
       </form>
     `,
   });
