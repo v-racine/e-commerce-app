9980ff7740a350b46d2bb089a88665fc18c3f994
Valerie Racine <vracine21@gmail.com>
2024-12-18 12:07:33 -0800

    refactor: html products views

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const signupTemplate = require('./views/admin/auth/signup');
 const signinTemplate = require('./views/admin/auth/signin');
 const productsNewTemplate = require('./views/admin/products/new');
 const productsIndexTemplate = require('./views/admin/products/index');
+
 const {
   parseEmail,
   parsePassword,
@@ -22,6 +23,7 @@ const {
   parsePrice,
   requireImage,
 } = require('./middlewares/parsers');
+const { requireAuth } = require('./middlewares/authenticator');
 
 const AppFactory = (args) => {
   // repos
@@ -109,6 +111,7 @@ const AppFactory = (args) => {
 
   app.post(
     '/admin/products/new',
+    //requireAuth,
     upload.single('image'),
     [parseTitle, parsePrice, requireImage],
     async (req, res) => {
