a340a7c7ca5ce57a42a961417bd264d260d74886
Valerie Racine <vracine21@gmail.com>
2024-12-20 16:04:49 -0800

    feat: shopping cart

--- src/services/cartsService.js
+++ src/services/cartsService.js
@@ -2,6 +2,18 @@ class CartsService {
   constructor(args) {
     this.cartsRepo = args.cartsRepo;
   }
+
+  async createCart() {
+    return await this.cartsRepo.create({ items: [] });
+  }
+
+  async retrieveCart(id) {
+    return await this.cartsRepo.getOne(id);
+  }
+
+  async updateCart(id, newObj) {
+    return await this.cartsRepo.update(id, newObj);
+  }
 }
 
 module.exports = {
